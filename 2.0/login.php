139ad80ac2dbcf9ce74e1c06c582b9477d39d62b
ZybsBaggins <202207207@post.au.dk>
2023-12-14 06:41:02 +0100

    Update login.php

--- 2.0/login.php
+++ 2.0/login.php
@@ -5,87 +5,34 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: rgba(0, 0, 0, 0.3); 
-        }
-        .container {
-        width: 90%;
-        margin: auto;
-        margin-top: 100px;
-        background-color: rgba(0, 0, 0, 0.5);
-        padding: 20px; 
-        border-radius: 10px; 
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.8); 
-        color: #fff;
-        }
-        .status {
-            margin-left: 2%;
-            margin-top: 5px;
-            width: 96.5%;
-            height: 50px;
-            text-align: center;
-            background-color: rgb(84,222,49, 0.5);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.8); 
-
-        }
-        .button-left{
-            margin-left: 2%;
-            margin-top: 10px;
-            width: 48%;
-            height: 300px;
-            text-align: center;
-            display: inline-block;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.8); 
-            
-
-        }
-        
-        .button-right{
-            margin-top: 10px;
-            width: 48%;
-            height: 300px;
-            text-align: center;
-            display: inline-block; 
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
-
-        }
-
-        .button-left:hover,
-        .button-right:hover {
-            background-color: rgba(255, 255, 255, 0.2); /* Change color on hover */
-            cursor: pointer; /* Change cursor on hover */
-        }
-
-        .button-left:active,
-        .button-right:active {
-            background-color: rgba(255, 255, 255, 0.4); /* Change color when clicked */
-        }
-
-    </style>
+    <!-- ... (your existing HTML head content) ... -->
 </head>
-
 <body>
-         LED Control:
-         <form method="get" action="gpio.php">
-                 <input type="submit" value="ON" name="on">
-                 <input type="submit" value="OFF" name="off">
-         </form>
-         <?php
-         $setmode18 = shell_exec("/usr/local/bin/gpio -g mode 18 out");
-         if(isset($_GET['on'])){
-                 $gpio_on = shell_exec("/usr/local/bin/gpio -g write 18 1");
-                 echo "LED is on";
-         }
-         else if(isset($_GET['off'])){
-                 $gpio_off = shell_exec("/usr/local/bin/gpio -g write 18 0");
-                 echo "LED is off";
-         }
-         ?>
-         </body>
-         
+    LED Control:
+    <form method="get" action="gpio.php">
+        <input type="submit" value="ON" name="on">
+        <input type="submit" value="OFF" name="off">
+    </form>
+
+    <?php
+    // Validate and sanitize user input
+    if (isset($_GET['on'])) {
+        // Ensure that 18 is the correct GPIO pin number for your LED
+        $gpio_on = shell_exec("/usr/local/bin/gpio -g write 18 1");
+        if ($gpio_on === false) {
+            echo "Error turning LED on";
+        } else {
+            echo "LED is on";
+        }
+    } elseif (isset($_GET['off'])) {
+        // Ensure that 18 is the correct GPIO pin number for your LED
+        $gpio_off = shell_exec("/usr/local/bin/gpio -g write 18 0");
+        if ($gpio_off === false) {
+            echo "Error turning LED off";
+        } else {
+            echo "LED is off";
+        }
+    }
+    ?>
+</body>
 </html>
