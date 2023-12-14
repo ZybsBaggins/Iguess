e502213d329746478f19b75527f468180957e429
ZybsBaggins <202207207@post.au.dk>
2023-12-14 07:11:10 +0100

    s

--- 2.0/login.php
+++ 2.0/login.php
@@ -14,5 +14,25 @@ session_start();
         <input type="submit" value="OFF" name="off">
     </form>
 
+    <?php
+    // Validate and sanitize user input
+    if (isset($_POST['on'])) {
+        // Ensure that 18 is the correct GPIO pin number for your LED
+        $gpio_on = shell_exec("/usr/local/bin/gpio -g write 18 1");
+        if ($gpio_on === false) {
+            echo "Error turning LED on";
+        } else {
+            echo "LED is on";
+        }
+    } elseif (isset($_POST['off'])) {
+        // Ensure that 18 is the correct GPIO pin number for your LED
+        $gpio_off = shell_exec("/usr/local/bin/gpio -g write 18 0");
+        if ($gpio_off === false) {
+            echo "Error turning LED off";
+        } else {
+            echo "LED is off";
+        }
+    }
+    ?>
 </body>
 </html>
