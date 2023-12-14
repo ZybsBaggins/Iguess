33b619bc6573c9a55e1b16287d7d318e5a570ca6
ZybsBaggins <202207207@post.au.dk>
2023-12-14 13:45:04 +0100

    Update index.php

--- 3.0/index.php
+++ 3.0/index.php
@@ -5,12 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST['on'])) {
         // Set the GPIO pin to HIGH (ON)
         shell_exec("/usr/bin/gpio -g write 18 1");
-        sleep(5);
-        shell_exec("/usr/bin/gpio -g write 18 0");
         
+        //Log
+        error_log("Button on pressed\n");
+
     } elseif (isset($_POST['off'])) {
         // Set the GPIO pin to LOW (OFF)
         shell_exec("/usr/bin/gpio -g write 18 0");
+        error_log("Button off pressed\n");
     }
 }
 ?>
